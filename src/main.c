d3421f6eb099830b84e8cef5d71916dc92598671
João Victor Araujo <jvsa@ic.ufal.br>
2021-09-09 22:50:58 -0300

    Atualizei o código pra retornar as chaves públicas
    Anthony precisa do valor das chaves públicas pra criar o arquivo, mas o meu código não retornava as chaves.
    Mas agora retorna

--- src/main.c
+++ src/main.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
-//Gerar chave (N, E), usando dois primos P e Q
-int chavesPublicas(){    
+/*Gerar chave (N, E), usando dois primos P e Q
+O argumento *chaves é necessário para retornar as chaves publicas 
+em um array, forma mais víavel que achei pra retornar a chave para
+a criação do arquivo .txt
+*/
+int chavesPublicas(int *chaves[2]){    
     int p, q, checPrimo, n, e;
 
     numPrimo1:
@@ -24,22 +28,24 @@ int chavesPublicas(){
         goto numPrimo2;
     }
 
-    n = p * q;
+    //n = p * q; Antiga implementação
+    chaves[0] = p * q; //armazena a chave n no par chaves
     
     expoente:
     printf("\nDigite um numero que seja coprimo a (p-1)*(q-1) para ser o expoente e: ");
     scanf("%d", &e);
     
-    e = expoenteE(e, p, q);
+    //e = expoenteE(e, p, q); Antiga implementação
+    chaves[1] = expoenteE(e, p, q); // armazena a chave e no par chaves
 
     if(e == 0){
         printf("\nNumero digitado nao eh coprimo a (p-1)*(q-1)!\n");
         goto expoente;
     }
 
-    printf("\np = %d\nq = %d\nn = %d\ne = %d\n", p, q, n, e); //printf apenas pra saber se os numeros estão sendo processados corretamente
+    //printf("\np = %d\nq = %d\nn = %d\ne = %d\n", p, q, n, e); printf apenas pra saber se os numeros estão sendo processados corretamente
     
-    return 0;
+    return chaves; //retorna o vetor chaves
 }
 
 /*
@@ -88,9 +94,10 @@ int numeroPrimo(int num){
 }    
 
 int main(){
+    int print[2]; // variável que recebe as chaves públicas
 
     printf("Seja bem vindo ao nosso RSA!!\n\n");
-    chavesPublicas();
+    chavesPublicas(&print);
 
     return 0;
 }
